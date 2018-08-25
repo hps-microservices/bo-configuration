e3e21b6bfa8d8a687a0f898c97dbda457228ef72
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-08-25 11:25:50 -0300

    Commit inicial

---
+++ src/main/java/com/msvvdv/boconfiguration/BoConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.msvvdv.boconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoConfigurationApplication.class, args);
+    }
+}
