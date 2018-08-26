2b75e0e22cd915926df03e2f44a411925c8e5bc8
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-08-26 12:40:05 -0300

    Create a server config and register in eureka server

--- src/main/java/com/msvvdv/boconfiguration/BoConfigurationApplication.java
+++ src/main/java/com/msvvdv/boconfiguration/BoConfigurationApplication.java
@@ -2,10 +2,12 @@ package com.msvvdv.boconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient // enable eureka client ..
 public class BoConfigurationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BoConfigurationApplication.class, args);
     }
